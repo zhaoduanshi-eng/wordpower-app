305c41d0efa6d68ee1629f736bc73a6b3ea221bf
zhaoduanshi-eng <zhaoduanshi@gmail.com>
2026-03-03 19:43:01 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,24 +1,19 @@
-window.chapters = {
-    sessions: {
-      session1: {
-        title: "Session 1",
-        word: {
-          term: "Egotist",
-          meaning: "自我中心的人"
-        },
-        quiz: [
-          {
-            question: "An egotist is someone who ____.",
-            options: [
-              "loves others",
-              "talks about himself constantly",
-              "is shy",
-              "avoids people"
-            ],
-            answer: 1
-          }
-        ]
-      }
+window.sessionData = {
+  title: "Session 1",
+  word: {
+    term: "Egotist",
+    meaning: "自我中心的人"
+  },
+  quiz: [
+    {
+      question: "An egotist is someone who ____.",
+      options: [
+        "loves others",
+        "talks about himself constantly",
+        "is shy",
+        "avoids people"
+      ],
+      answer: 1
     }
-  }
+  ]
 };
